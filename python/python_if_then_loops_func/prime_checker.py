4e9e5a968f9b01ad726c8feb713c21bbb5ee676d
OrelNeto-DO <orelnetodo@gmail.com>
2024-11-17 01:33:53 +0200

    another change to primes

--- python/python_if_then_loops_func/prime_checker.py
+++ python/python_if_then_loops_func/prime_checker.py
@@ -1,4 +1,20 @@
-import sys
+# import sys
+
+# def is_prime(num):
+#     if num <= 1:
+#         print("all prime numbers are greater than one")
+#         return False
+
+#     for i in range(2, int((num ** 0.5) +1)):
+#         if num % i == 0:
+#             print(f"the number {num} is not prime")
+#             return False
+#     print(f"the number {num} is prime")
+#     return True
+
+# is_prime(11)
+
+# import sys
 
 def is_prime(num):
     if num <= 1:
@@ -7,7 +23,11 @@ def is_prime(num):
 
     for i in range(2, int((num ** 0.5) +1)):
         if num % i == 0:
+            print(f"the number {num} is not prime")
             return False
     print(f"the number {num} is prime")
     return True
-is_prime(7)
+
+test_numbers = [7, 10, 13, 5, 7, 9, 11, 13, 15, 21, 30]
+for number in test_numbers:
+    print(is_prime(number))
