4a5b1f19ffabbe3101fd2d9446831fad47ebad10
OrelNeto-DO <orelnetodo@gmail.com>
2024-12-02 03:33:18 +0200

    done with class exercise

--- python/classes_excersice/first_self_classes.py
+++ python/classes_excersice/first_self_classes.py
@@ -5,7 +5,7 @@ class Person:
         self.age = age
 
     def introduce(self):
-        return f"Hi, i'm {self.name} and i'm {self.age} years old"
+        return f"Hi, i'm {self.name} and i'm {self.age} years old. my grade for the current semester is '{self.grade}'."
 
 class Student(Person):
 
@@ -13,9 +13,27 @@ class Student(Person):
         super().__init__(name, age)
         self.grade = grade
 
-    def study(self, proffesion):
-        return f"{self.name} is studying python classes right now"
+    def study(self, profession):
         self.profession = profession
+        return f"{self.name} is studying {self.profession} classes right now"
 
-Student("orel", 25).introduce("orel", 25).study("python")
-        
+student1 = Student("orel", 25, "A")
+print(student1.introduce())
+print(student1.study("python"))
+
+student2 = Student("random", 23, "B")
+print(student2.introduce())
+print(student2.study("biology"))
+
+student3 = Student("plankton", 27, "A")
+print(student3.introduce())
+print(student3.study("pharmacy"))
+
+studnet4 = Student("mister crab", 47, "A")
+print(studnet4.introduce())
+print(studnet4.study("how to cook anything but burgers in his"))
+
+student5 = Student("emil,", 26, "C")
+print(student5.introduce())
+print(student5.study("CyberSecurity"))
+print(f"{student5.name} is thinking about replacing his career into {student5.study('DevOps').split('is ')[1].split(' ')[1]} engineer")
